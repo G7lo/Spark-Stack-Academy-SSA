30f57bdf63dfed705bf956893df60bb4f3f51221
SparkStack Tech <phinehaseregae724@gmail.com>
2026-08-22 06:02:03 +0300

    Enable premium splash across Firebase-powered pages

--- js/firebase.js
+++ js/firebase.js
@@ -1,5 +1,6 @@
 import "./ui-runtime.js";
 import "./ui-polish.js";
+import "./splash-screen.js";
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
