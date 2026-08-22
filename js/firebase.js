3e7a1f03395c623b6d463d3cf5354731e9a11a12
SparkStack Tech <phinehaseregae724@gmail.com>
2026-08-22 10:13:07 +0300

    Update Firebase config for SSA Web Client V2

--- js/firebase.js
+++ js/firebase.js
@@ -8,13 +8,14 @@ import { getAuth, setPersistence, browserLocalPersistence } from "https://www.gs
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBlPs-9EU_YYiP4qZ6gFF9ZorJbbktqC4",
+    apiKey: "AIzaSyBlPs-9EU_YYiP4qZ6gFF9ZorJbbktxC4",
     authDomain: "spark-stack-academy.firebaseapp.com",
+    databaseURL: "https://spark-stack-academy-default-rtdb.firebaseio.com",
     projectId: "spark-stack-academy",
     storageBucket: "spark-stack-academy.firebasestorage.app",
     messagingSenderId: "691304828755",
-    appId: "1:691304828755:web:b78b4c47ad007a5be39ba6",
-    measurementId: "G-2CJ4RW1PNZ"
+    appId: "1:691304828755:web:dcb92043eb1a308ce39ba6",
+    measurementId: "G-GYS3P7ZVYJ"
 };
 
 const app = initializeApp(firebaseConfig);
