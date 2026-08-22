363a23092ba86ad59a59f6336792cf780e4afcc8
G7lo <phinehaseregae724@gmail.com>
2026-08-22 14:36:09 +0300

    Restore UI runtime initialization

--- js/firebase.js
+++ js/firebase.js
@@ -1,52 +1,41 @@
+import "./ui-runtime.js";
+import "./ui-polish.js";
+import "./splash-screen.js";
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-import { 
+import {
     getAuth,
     setPersistence,
     browserLocalPersistence
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-
 import {
     getStorage
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 
-
 const firebaseConfig = {
-
     apiKey: "AIzaSyBlPs-9EU_YYiP4qZ6gFF9ZorJbbktXqC4",
-
     authDomain: "spark-stack-academy.firebaseapp.com",
-
     projectId: "spark-stack-academy",
-
     storageBucket: "spark-stack-academy.firebasestorage.app",
-
     messagingSenderId: "691304828755",
-
     appId: "1:691304828755:web:b78b4c47ad007a5be39ba6",
-
     measurementId: "G-2CJ4RW1PNZ"
-
 };
-
 
 const app = initializeApp(firebaseConfig);
 
 const db = getFirestore(app);
-
 const auth = getAuth(app);
-
 const storage = getStorage(app);
 
-// Keep users logged in after refresh
 setPersistence(
     auth,
     browserLocalPersistence
 );
-
 
 export {
     auth,
